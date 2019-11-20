94a2566607b96cb4eff97e8934127fd23a746f81
Pedro Martins <57993788+bbpedromartins@users.noreply.github.com>
2019-11-20 14:14:15 +0000

    Adding some colors.

--- o11_core.js
+++ o11_core.js
@@ -8,3 +8,26 @@
 * LICENSE       : 
 *********************************************
 */
+
+"use strict";
+
+const RED      = "rgba(255,000,000,1.0)";
+const LIME     = "rgba(000,255,000,1.0)";
+const BLUE     = "rgba(000,000,255,1.0)";
+const BLACK    = "rgba(000,000,000,1.0)";
+const WHITE    = "rgba(255,255,255,1.0)";
+const YELLOW   = "rgba(255,255,000,1.0)";
+const CYAN     = "rgba(000,255,255,1.0)";
+const AQUA     = "rgba(000,255,255,1.0)";
+const MAGENTA  = "rgba(255,000,255,1.0)";
+const FUCHSIA  = "rgba(255,000,255,1.0)";
+const SILVER   = "rgba(192,192,192,1.0)";
+const GRAY     = "rgba(128,128,128,1.0)";
+const GREY     = "rgba(128,128,128,1.0)";
+const MARROON  = "rgba(128,000,000,1.0)";
+const OLIVE    = "rgba(128,128,000,1.0)";
+const GREEN    = "rgba(000,128,000,1.0)";
+const PURPLE   = "rgba(128,000,128,1.0)";
+const TEAL     = "rgba(000,128,128,1.0)";
+const NAVY     = "rgba(000,000,128,1.0)";
+const ORANGE   = "rgba(255,165,000,1.0)";
