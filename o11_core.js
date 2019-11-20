6d66cb59aa00d260f21a868f318dc50075a330ed
Pedro Martins <57993788+bbpedromartins@users.noreply.github.com>
2019-11-20 13:33:00 +0000

    adding project info

---
+++ o11_core.js
@@ -0,0 +1,10 @@
+/*
+*********************************************
+* LIBRARY NAME  : OB11_CORE
+* VERSION       : 20.11.2019
+* AUTHOR/CODER  : Pedro Martins
+* FILE NAME     : ob11_core.js
+* SITE          : 
+* LICENSE       : 
+*********************************************
+*/
