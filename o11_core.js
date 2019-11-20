f070dc66631f350d6a01138c8415f6a56e100d80
Pedro Martins <57993788+bbpedromartins@users.noreply.github.com>
2019-11-20 14:19:42 +0000

    Update o11_core.js

--- o11_core.js
+++ o11_core.js
@@ -3,7 +3,7 @@
 * LIBRARY NAME  : OB11_CORE
 * VERSION       : 20.11.2019
 * AUTHOR/CODER  : Pedro Martins
-* FILE NAME     : ob11_core.js
+* FILE NAME     : o11_core.js
 * SITE          : 
 * LICENSE       : 
 *********************************************
