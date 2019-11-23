ad8d1cacbc5d8e1cddf1a50d8966dcc155df5e28
Pedro Martins <57993788+bbpedromartins@users.noreply.github.com>
2019-11-23 16:11:11 +0000

    Add files via upload

---
+++ o11.js
@@ -0,0 +1,22 @@
+
+/*
+*********************************************
+* LIBRARY NAME  : O11
+* VERSION       : 20.11.2019
+* AUTHOR/CODER  : Pedro Martins
+* FILE NAME     : o11.js
+* SITE          : obj11.com
+* LICENSE       : obj11.com
+*********************************************
+*/
+
+//change this to your O11 js path in your website
+let o11path='/site/js/';
+
+// include core
+document.writeln("<script src='"+o11path+"o11_core.js'></script>");
+
+document.writeln("<script src='"+o11path+"objects/o11_page.js'></script>");
+document.writeln("<script src='"+o11path+"objects/o11_tile.js'></script>");
+document.writeln("<script src='"+o11path+"objects/o11_audio.js'></script>");
+
