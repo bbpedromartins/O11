701a5f796df1b6ad848e4035cda14845cb1841e6
Pedro Martins <57993788+bbpedromartins@users.noreply.github.com>
2020-01-04 01:22:22 +0000

    Add files via upload

---
+++ o11.js
@@ -0,0 +1,26 @@
+/*
+*********************************************
+* O11 
+*********************************************
+*/
+
+//change this to your O11 path in your website
+let o11path='/site/o11/';
+
+// include core
+document.writeln("<script src='"+o11path+"o11_colors.js'></script>");
+document.writeln("<script src='"+o11path+"o11_core.js'></script>");
+
+document.writeln("<script src='"+o11path+"objects/o11_page.js'></script>");
+document.writeln("<script src='"+o11path+"objects/o11_tile.js'></script>");
+document.writeln("<script src='"+o11path+"objects/o11_audio.js'></script>");
+document.writeln("<script src='"+o11path+"objects/o11_text.js'></script>");
+document.writeln("<script src='"+o11path+"objects/o11_image.js'></script>");
+document.writeln("<script src='"+o11path+"objects/o11_include.js'></script>");
+document.writeln("<script src='"+o11path+"objects/o11_textlink.js'></script>");
+document.writeln("<script src='"+o11path+"objects/o11_imagelink.js'></script>");
+document.writeln("<script src='"+o11path+"objects/o11_textlist.js'></script>");
+document.writeln("<script src='"+o11path+"objects/o11_textlistlink.js'></script>");
+document.writeln("<script src='"+o11path+"objects/o11_video.js'></script>");
+document.writeln("<script src='"+o11path+"objects/o11_videolink.js'></script>");
+
